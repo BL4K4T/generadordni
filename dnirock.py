249a07e14bd2de979574683c5bfc8e262b698bd7
BL4K4T <76046799+BL4K4T@users.noreply.github.com>
2021-05-28 18:05:58 +0100

    Add files via upload

---
+++ dnirock.py
@@ -0,0 +1,33 @@
+
+#!/usr/bin/env python
+
+#Generador DNI
+
+import random
+import string
+
+
+
+#Generador del número de DNI
+
+def randNum(num):
+
+	return "".join(random.choice(string.digits) for t in range(8))
+
+
+#Generador de letra en base al número generado
+
+def dni(letra):
+
+	seq = 'TRWAGMYFPDXBNJZSQVHLCKE'
+	numero=randNum(letra)
+	
+	return numero + seq[int(numero) % 23] + "\n"
+
+
+#Escritura del archivo
+
+with open("dni.txt","w") as f:
+	for i in range(int(input('\nNumero de DNIs a generar: '))):
+		f.write(dni(i))
+f.closed
